e96c26baa95df693decbe36d4f6ed046cf26a3a8
jedrzej makuch <jedrzejmakuch@gmail.com>
2025-09-18 20:43:54 +0200

    Add Fact page with fetched data

--- Purrfect-Blog-Starter/Controllers/CatController.cs
+++ Purrfect-Blog-Starter/Controllers/CatController.cs
@@ -1,19 +1,44 @@
-﻿using System.Web.Mvc;
+﻿using Newtonsoft.Json;
+using Purrfect_Blog_Starter.Dtos;
+using Purrfect_Blog_Starter.Utilities;
+using Purrfect_Blog_Starter.ViewModels;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace Purrfect_Blog_Starter.Controllers
 {
     public class CatController : Controller
     {
+        private static readonly HttpClient _http = new HttpClient();
+        private const string FactsUrl = "https://catfact.ninja/facts?limit=20";
+
         [AllowAnonymous]
-        public ActionResult Index()
-        {
-            return View();
-        }
+        public ActionResult Index() => View();
 
         [Authorize]
-        public ActionResult Dashboard()
+        public async Task<ActionResult> Facts()
         {
-            return View();
+            var vm = new CatFactsViewModel();
+            var facts = await FetchFactsAsync();
+
+            vm = ViewModelMapper.ToCatFactsViewModel(facts);
+
+            return View(vm);
+        }
+
+        private async Task<CatFactsDto> FetchFactsAsync()
+        {
+            var resp = await _http.GetAsync(FactsUrl);
+
+            if (!resp.IsSuccessStatusCode)
+            {
+                return new CatFactsDto { Data = new List<CatFactDto>() };
+            }
+
+            var json = await resp.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<CatFactsDto>(json);
         }
     }
 }
---
+++ Purrfect-Blog-Starter/ViewModels/CatFactsViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Purrfect_Blog_Starter.ViewModels
+{
+    public class CatFactsViewModel
+    {
+        public List<CatFactViewModel> Facts { get; set; } = new List<CatFactViewModel>();
+    }
+}
